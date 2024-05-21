27b9b09316e90d37e5939fb5c8c5366e62a9de76
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2024-05-22 00:13:14 +0900

    add recipe page with alphabet

---
+++ app/recipe/[f]/recipe.tsx
@@ -0,0 +1,14 @@
+export default function Recipe(drink: any) {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md w-full max-w-md">
+      <img
+        src={drink.strDrinkThumb}
+        alt={drink.strDrink}
+        className="w-full h-auto mb-4 rounded-md"
+      />
+      <span className="text-xl text-black font-semibold mb-2">
+        {drink.strDrink}
+      </span>
+    </div>
+  );
+}
--- app/recipe/page.tsx
+++ app/recipe/page.tsx
@@ -1,3 +1,29 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Recipe() {
-  return <div>Recipe</div>;
+  const router = useRouter();
+  const alphabets = "abcdefghijklmnopqrstuvwxyz".split("");
+
+  const handleAlphabetClick = (alphabet: string) => {
+    router.push(`/recipe/${alphabet}`);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold mb-6">最初のアルファベットを選択</h1>
+      <div className="grid grid-cols-6 gap-4">
+        {alphabets.map((alphabet) => (
+          <button
+            key={alphabet}
+            onClick={() => handleAlphabetClick(alphabet)}
+            className="bg-red-500 text-white rounded-lg py-2 px-4 hover:bg-red-600 focus:outline-none"
+          >
+            {alphabet.toUpperCase()}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 }
