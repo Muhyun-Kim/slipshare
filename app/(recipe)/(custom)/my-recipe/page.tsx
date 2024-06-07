51764a3640ae8bccb7e0ffc52184e6c00a93a33d
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2024-06-08 00:59:49 +0900

    add delete my recipe

--- app/(recipe)/(custom)/my-recipe/page.tsx
+++ app/(recipe)/(custom)/my-recipe/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { PlusCircleIcon } from "@heroicons/react/20/solid";
+import { PlusCircleIcon, MinusCircleIcon } from "@heroicons/react/20/solid";
 import { CustomRecipe } from "@prisma/client";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { fetchMyRecipes } from "./actions";
+import { deleteMyRecipe, fetchMyRecipes } from "./actions";
 import useUserStore from "@/store/useUserStore";
 import Image from "next/image";
 
@@ -20,6 +20,12 @@ export default function MyRecipePage() {
     getMyRecipes();
   }, []);
 
+  const handleDeleteMyRecipe = async (id: number) => {
+    deleteMyRecipe(id);
+    const newRecipes = myRecipes.filter((recipe) => recipe.id !== id);
+    setMyRecipes(newRecipes);
+  };
+
   return (
     <div className="flex justify-center">
       <div className="flex flex-col pt-8 items-center min-h-screen w-3/4">
@@ -29,15 +35,19 @@ export default function MyRecipePage() {
           </button>
         </Link>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {myRecipes.map((myRecipe) => {
-            console.log(myRecipe.id);
+          {myRecipes.map((customRecipe) => {
             return (
-              <div className="flex flex-col justify-between p-4 bg-white rounded-lg shadow-md w-full max-w-md">
-                {myRecipe.cocktailImg !== "" ? (
+              <div className="flex flex-col justify-around p-4 bg-white rounded-lg shadow-md w-full max-w-md">
+                <div className="flex justify-end">
+                  <button onClick={() => handleDeleteMyRecipe(customRecipe.id)}>
+                    <MinusCircleIcon className="h-8 w-8 text-red-500" />
+                  </button>
+                </div>
+                {customRecipe.cocktailImg !== "" ? (
                   <>
                     <img
-                      src={myRecipe.cocktailImg!}
-                      alt={myRecipe.cocktailName}
+                      src={customRecipe.cocktailImg!}
+                      alt={customRecipe.cocktailName}
                       className="w-full h-auto mb-4 rounded-md"
                     />
                   </>
@@ -45,7 +55,7 @@ export default function MyRecipePage() {
                   <>
                     <Image
                       src="/image/sample-cocktail.png"
-                      alt={myRecipe.cocktailName}
+                      alt={customRecipe.cocktailName}
                       width={50}
                       height={50}
                       className="w-full h-auto mb-4 rounded-md"
@@ -53,7 +63,7 @@ export default function MyRecipePage() {
                   </>
                 )}
                 <span className="text-xl text-black font-semibold mb-2">
-                  {myRecipe.cocktailName}
+                  {customRecipe.cocktailName}
                 </span>
               </div>
             );
